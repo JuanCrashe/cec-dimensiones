52159fc37eeb70c8d664d0f928e17773e24f1d52
Juan Carlos Cela <juan.crashe@live.com>
2024-11-11 23:14:49 -0500

    Configurar puerto para Koyeb

--- run.py
+++ run.py
@@ -12,4 +12,4 @@ def make_shell_context():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=8080)
